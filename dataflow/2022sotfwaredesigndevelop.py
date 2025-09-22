92b7ef15748e6cc790fe8ab5e604f0807a03755d
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2025-09-22 14:50:54 +0000

    glub glub glub

--- dataflow/2022sotfwaredesigndevelop.py
+++ dataflow/2022sotfwaredesigndevelop.py
@@ -1,14 +1,15 @@
 import csv
-
-def newmember():
+#Function to get information of a new member
+def newmember(): 
     firstname = input("enter first name: ")
     lastname = input("enter last name: ")
     category= input("Enter category- Junior, adult, senior: ")
     password=input("enter password: ")
     return firstname, lastname, category, password
 
-
+# procedure to read csv file into paralel arrays
 def readdisplayfile(firstname, lastname, category, password):
+    #opens csv file in read mode
     with open("dataflow/members.csv", "r") as file:
           firstname=[firstname]
           lastname=[lastname]
@@ -23,8 +24,8 @@ def readdisplayfile(firstname, lastname, category, password):
 
     return category
 
-
-def numbercategory(category):
+#procedure to count amount of people in specific category
+def numbercategory(category): 
      search = input("what category to count- Junior, Adult, Senior: ")
      counter=0
      for x in range (len(category)):
@@ -32,10 +33,12 @@ def numbercategory(category):
                counter=counter+1
      print(counter)
 
+#procedure to check if password is valid or not
 def validpassword(password):
      valid=False
      while not valid:
           password=input("enter password: ")
+          #checks if password has any capital letters at start and if it has a symbol at the end of the password using unicode values
           if ord(password[0]) >=65 and ord(password[0])<=90 and ord(password[-1])>=35 and ord(password[-1])<=37:
                print("valid")
                valid = True
@@ -45,7 +48,7 @@ def validpassword(password):
 
 
 
-
+#main program
 globalfirstname, globallastname, globalcategory, globalpassword = newmember()
 globalcategory=readdisplayfile(globalfirstname, globallastname, globalcategory, globalpassword)
 numbercategory(globalcategory)
--- dataflow/2023softwaredevelope.py
+++ dataflow/2023softwaredevelope.py
@@ -1,5 +1,6 @@
 import csv
 
+#function to read csv file into paralell arrays 
 def readfile():
     with open("dataflow/attractions.csv", "r") as file:
         attraction=[]
@@ -11,10 +12,39 @@ def readfile():
         for row in reader:
             attraction.append(row[0])
             category.append(row[1])
-            visitors.append(row[2])
-            daysOpen.append(row[3])
+            visitors.append(int(row[2]))
+            daysOpen.append(int(row[3]))
             height.append(row[4])
         return attraction, category, visitors, daysOpen, height
-    
+
+
+# procedure to find the attractions with most/least amount of visitors
 def leastmostattract(attraction, visitors):
-    mostattract = 
+    leasvisited = 0
+    mostvisited = 0
+    for x in range(len(visitors)):
+        if visitors[x] < visitors[leasvisited]:
+            leasvisited = x
+        if visitors[x] > visitors[mostvisited]:
+            mostvisited = x
+    print("the most visited attraction is: " + attraction[mostvisited])
+    print("the least visited attraction is: " + attraction[leasvisited])
+
+def needservice(attraction, category, daysOpen):
+    with open("dataflow/service.csv", "w") as file:
+        for x in range(len(category)):
+          if category[x] == "Roller Coaster":
+              days = daysOpen[x] % 90
+              if (90-days) <= 7:
+                  file.write(attraction[x])
+                  
+                  
+              
+
+    
+
+
+#main program
+globalattraction, globalcategory, globalvisitors, globaldaysopen, globalheight = readfile()
+leastmostattract(globalattraction, globalvisitors)
+needservice(globalattraction, globalcategory, globaldaysopen)
