efbaf53f92e749ace8f21f805625cd618313b0d0
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2025-11-10 15:48:47 +0000

    ddddd

--- Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
+++ Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
@@ -1,1 +1,13 @@
-DESCRIBE ALL TABLES;
+-- question 2c
+-- SELECt s.initial, s.surname, s.swimCategory, t.teamName, COUNT(r.position) AS 'Races won'
+-- FROM Swimmer s
+-- JOIN Team t ON t.teamRef = s.teamRef
+-- JOIN Result r ON r.swimmerID = s.swimmerID
+-- WHERE r.position = 1
+-- GROUP BY s.swimmerID
+-- ORDER BY s.initial ASC;
+
+SELECT s.initial, s.surname, t.teamName, e.city, e.eventDate
+FROM Event e  
+JOIN Race rac ON e.eventID = rac.eventID
+JOIN Result res ON 
