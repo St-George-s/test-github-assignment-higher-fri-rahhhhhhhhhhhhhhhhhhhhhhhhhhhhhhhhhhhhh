bfd44857b683dffb3e6c3f6702ece75a92ccd59d
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2025-11-13 10:25:43 +0000

    yeah ..........

--- Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
+++ Courseworks Databases/SQA-24-Coursework-Database-Swimming/main.sql
@@ -7,7 +7,30 @@
 -- GROUP BY s.swimmerID
 -- ORDER BY s.initial ASC;
 
-SELECT s.initial, s.surname, t.teamName, e.city, e.eventDate
-FROM Event e  
-JOIN Race rac ON e.eventID = rac.eventID
-JOIN Result res ON 
+
+--2D
+-- SELECT s.initial, s.surname, t.teamName, e.city, e.eventDate
+-- FROM Event e  
+-- JOIN Race rac ON e.eventID = rac.eventID
+-- JOIN Result res ON rac.raceNumber = res.raceNumber
+-- JOIN Swimmer s ON res.swimmerID = s.swimmerID
+-- JOIN Team t ON s.teamRef = t.teamRef
+-- WHERE res.raceTime = (
+--     SELECT min(raceTime)
+--     FROM Result
+--     WHERE lane = 1 or lane = 8
+-- );
+
+
+--2e
+-- SELECT teamName, COUNT(position) AS [Total medals won]
+-- FROM Result, Swimmer, Team
+-- WHERE Result.swimmerID = Swimmer.swimmerID 
+-- AND Swimmer.teamRef = Team.teamRef
+-- AND position <4
+-- GROUP BY teamName
+-- ORDER BY COUNT(position) DESC;
+
+
+SELECT *
+FROM Event;
