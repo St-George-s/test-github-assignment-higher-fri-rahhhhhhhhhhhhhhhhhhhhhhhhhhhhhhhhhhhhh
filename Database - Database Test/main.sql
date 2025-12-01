35882f8f4e1e6628715e66afc2b3f1f8b90a4d81
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2025-12-01 15:23:13 +0000

    lowkey test

--- Database - Database Test/main.sql
+++ Database - Database Test/main.sql
@@ -1,1 +1,18 @@
-DESCRIBE ALL TABLES;
+-- 3.1
+-- SELECT e.episodeTitle, s.showName, e.maxViewers, e.episodeDate
+-- FROM Episode e
+-- JOIN Show s ON e.showID = s.showID;
+
+
+-- 3.2
+-- SELECT s.showName AS 'Show', e.episodeTitle AS 'Episode'
+-- FROM Episode e
+-- JOIN Show s ON e.showID = s.showID
+-- WHERE e.episodeDate LIKE '2024-12-%';
+
+
+--3.3
+-- UPDATE Timeslot
+-- SET endTime = '19:30'
+-- WHERE showID = '2';
+
