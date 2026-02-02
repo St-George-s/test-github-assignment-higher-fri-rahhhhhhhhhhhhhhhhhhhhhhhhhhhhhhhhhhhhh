193eda3fdd95d55a11c7d5e8f55bb39fd11c4634
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2026-02-02 16:10:47 +0000

    hello

--- Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
+++ Courseworks Databases/SQA-Sample-Coursework-Database/main.sql
@@ -1,1 +1,17 @@
-SELECT * FROM Voucher;
+-- SELECT c.firstName, c.surname, v.voucherID, ROUND(co.quantityPurchased * v.price) AS "Amount of Money Spent on Voucher £"
+-- FROM Customer c
+-- JOIN CustomerOrder co ON c.custID = co.custID
+-- JOIN Voucher v ON co.voucherID = v.voucherID
+-- WHERE v.category = "Adventure"
+-- AND v.voucherName LIKE "%escape room%"
+-- GROUP BY co.custID
+-- ORDER BY v.voucherID ASC;
+
+
+-- SELECT v.voucherID, s.supplierName, v.voucherName, v.quantityAvailable - SUM(co.quantityPurchased) AS "Still Available"
+-- FROM Voucher v
+-- JOIN Supplier s ON v.supplierCode = s.supplierCode
+-- JOIN CustomerOrder co ON v.voucherID = co.voucherID
+-- WHERE v.voucherID = "V543";
+
+
