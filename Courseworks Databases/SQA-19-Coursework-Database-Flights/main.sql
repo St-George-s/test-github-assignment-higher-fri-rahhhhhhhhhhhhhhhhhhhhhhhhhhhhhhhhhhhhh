393f627cf197e27180fbecfb3f4b3b6b0ff5fd1c
rahhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh <mac128@stge.org.uk>
2026-02-05 10:30:53 +0000

    r

--- Courseworks Databases/SQA-19-Coursework-Database-Flights/main.sql
+++ Courseworks Databases/SQA-19-Coursework-Database-Flights/main.sql
@@ -1,1 +1,13 @@
-DESCRIBE ALL TABLES;
+-- SELECT c.forename, c.surname, (b.adultTicket*5.50)+(b.childTicket *2.0)+(b.concessionTicket*1.5) AS "Tax (£)"
+-- FROM Customer c
+-- JOIN Booking b ON c.customerID = b.customerID
+-- WHERE c.customerID = "GR01932" AND b.flightID = "QH182";
+
+SELECT MAX(childTicket) AS "maxChildren"
+FROM Booking
+
+SELECT c.forename, c.surname
+FROM Customer c 
+JOIN Booking b ON c.customerID = b.customerID
+WHERE b.childTicket
+GROUP BY c.customerID;
